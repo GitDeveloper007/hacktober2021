74a65d87000c5306dba1568ed9165e91bcceca4a
Git-Aviral <72214917+Git-Aviral@users.noreply.github.com>
2021-10-16 20:36:14 +0530

    Update Number of pairs.cpp

--- Number of pairs.cpp
+++ Number of pairs.cpp
@@ -31,3 +31,15 @@ Constraints:
 1 ≤ M, N ≤ 105
 1 ≤ X[i], Y[i] ≤ 103
 */
+
+long long countPairsBruteForce(long long X[], long long Y[],
+							long long m, long long n)
+{
+	long long ans = 0;
+
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			if (pow(X[i], Y[j]) > pow(Y[j], X[i]))
+				ans++;
+	return ans;
+}
